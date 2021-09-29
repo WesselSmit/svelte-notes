921d2d565c2202eb54a1dd047b166b2686937027
wesselsmit <wrsmit00@gmail.com>
2021-09-29 13:54:22 +0200

    feat: make note-rows fill the remaining height

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -1,12 +1,63 @@
 <main class="app">
-
+  <section class="note">
+    <ul class="note__list">
+      {#each notes as note}
+        <li class="note-row">
+          <textarea
+            type="text"
+            class="note-row__input {note.type}"
+            value={note.content}
+            placeholder={note.type === 'new' ? 'Start typing..' : null}
+          />
+        </li>
+      {/each}
+    </ul>
+  </section>
 </main>
 
 <script>
+  const noteFromStorage = [
+    {
+      type: 'body',
+      content: 'This is some note content'
+    },
+    {
+      type: 'body',
+      content: 'This is some more note content'
+    },
+    {
+      type: 'body',
+      content: 'This is some other note content'
+    }
+  ]
+  // const noteFromStorage = []
+  const defaultNote = [{ type: 'new', content: '' }]
+  const notes = noteFromStorage.length ? noteFromStorage : defaultNote
+
 
 </script>
 
 <style lang="scss">
+.note {
+  &__list {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    list-style: none;
 
+    & > *:last-child {
+      flex: 1;
+    }
+  }
+}
+
+.note-row {
+  &__input {
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
 
